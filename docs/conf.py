56053ae5365f83016517bbf0c0490a2093cd6c83
n4p3r <n4p3r@localhost.localdomain>
2017-01-23 15:50:15 +0100

    removing githubpages

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,7 @@
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
